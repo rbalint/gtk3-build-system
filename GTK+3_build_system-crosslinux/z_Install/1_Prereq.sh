167c39ce3577e63e6d14543b182e6ff6b448650b
Balint Reczey <balint@balintreczey.hu>
2015-06-24 17:46:21 +0200

    Add script for installing prerequisites on CentOS 7

---
+++ GTK+3_build_system-crosslinux/z_Install/1_Prereq.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## install needed packages assuming CentOS 7
+yum install -y epel-release
+yum install -y mingw32-gcc mingw64-gcc git patch make autoconf automake gcc gperf gettext wget bison tar libtool which
