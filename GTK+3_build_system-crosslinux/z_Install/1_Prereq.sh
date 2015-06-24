75a1932d631a21778c775ef829bb1353d5d311d2
Balint Reczey <balint@balintreczey.hu>
2015-06-25 00:00:54 +0200

    Add new prerequisites

--- GTK+3_build_system-crosslinux/z_Install/1_Prereq.sh
+++ GTK+3_build_system-crosslinux/z_Install/1_Prereq.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 ## install needed packages assuming CentOS 7
+cd /root
 yum install -y epel-release
-yum install -y mingw32-gcc mingw64-gcc git patch make autoconf automake gcc gperf gettext wget bison tar libtool which
+yum install -y mingw32-gcc mingw64-gcc git patch make autoconf automake gcc gperf gettext wget bison tar libtool which libxslt-devel python-lxml bzip2 gcc-c++
+wget ftp://fr2.rpmfind.net/linux/sourceforge/k/ke/kenzy/special/C7/x86_64/ragel-6.8-3.el7.centos.x86_64.rpm
+# TODO check sha1sum
+rpm -i ragel-6.8-3.el7.centos.x86_64.rpm
+
